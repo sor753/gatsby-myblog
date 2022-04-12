d7458c3d0a7262f910e063f1d32ae318c5bbf5b9
show kawazoe <show753@icloud.com>
2022-04-12 14:31:43 +0900

    modularize layout

---
+++ src/components/layout.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+export default ({ children }) => (
+  <div>
+    <Header />
+
+    { children }
+
+    <Footer />
+  </div> 
+)
