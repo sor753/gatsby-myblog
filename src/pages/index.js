86cb6bb4f0d4cacdbe94db7c1d1a9199efc40b03
show kawazoe <show753@icloud.com>
2022-04-12 13:05:49 +0900

    gatsby-image component clipping

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,10 @@ const IndexPage = ({ data }) => {
 			</header>
 			<section className="hero">
 				<figure>
-					<Img fluid={data.hero.childImageSharp.fluid} alt="" />
+					<Img fluid={data.hero.childImageSharp.fluid}
+            alt=""
+            style={{ height: "100%" }}
+          />
 				</figure>
 				<div className="catch">
 					<h1>There is no love sincerer than<br /> the love of food.</h1>
@@ -67,7 +70,10 @@ const IndexPage = ({ data }) => {
 			<section className="photo">
 				<h2 className="sr-only">Photo</h2>
 				<figure>
-          <Img fluid={data.berry.childImageSharp.fluid} alt="" />
+          <Img fluid={data.berry.childImageSharp.fluid}
+            alt="" 
+            style={{ height: "100%" }}
+          />
 				</figure>
 			</section>
 			<footer className="footer">
