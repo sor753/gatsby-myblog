d2681c2fa07c004a490675183fb97536da6fe93b
show kawazoe <show753@icloud.com>
2022-04-12 15:25:01 +0900

    add fontawesome icons in about.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,10 @@ import Footer from '../components/footer'
 
 import './layout.css'
 
+import '@fortAwesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortAwesome/fontawesome-svg-core'
+config.atoAddCss = false
+
 export default ({ children }) => (
   <div>
     <Header />
--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,9 @@ import Img from "gatsby-image"
 
 import Layout from '../components/layout'
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faUtensils, faCheckSquare } from "@fortawesome/free-solid-svg-icons"
+
 // markup
 export default ({ data }) => (
   <Layout>
@@ -21,20 +24,24 @@ export default ({ data }) => (
 					<h1 className="bar">ESSENTIALSについて</h1>
 					<aside className="info">
 						<div className="subtitle">
-							<i className="fas fa-utensils" />
+						<FontAwesomeIcon icon={faUtensils} />
 							ABOUT ESSENTIALS
 						</div>
 					</aside>
 					<div className="postbody">
 						<p>体に必要不可欠な食べ物についての情報を発信しているサイトです。「おいしい食材をおいしく食べる」をモットーにしています。特に力を入れているのが、フルーツ、穀物、飲み物の３つです。</p>
-						<h2><i className="fas fa-check-square" />公開している記事</h2>
+						<h2>
+							<FontAwesomeIcon icon={faCheckSquare} />
+						</h2>
 						<p>これらについて、次のような記事やレシピなどを書いて公開しています。</p>
 						<ul>
 							<li>ヘルシーで美味しい料理の研究・レビュー・レシピ。</li>
 							<li>一人でも、家族や友人と一緒にでも楽しめる料理。</li>
 							<li>ユーザー間のオープンなコミュニケーションを基盤とした情報。</li>
 						</ul>
-						<h2><i className="fas fa-check-square" />よく聞かれること</h2>
+						<h2>
+							<FontAwesomeIcon icon={faCheckSquare} />
+						</h2>
 						<p>よく聞かれることなのですが、私たちはスタートアップではありません。私たちはまだスタートしたばかりで、より多くの人々が食べやすい食べ物がもたらす違いを発見できるように、成長しサービスを改善する機会を待っています。</p>
 						<p>気長にお付き合いいただければ幸いです。</p>
 					</div>
