1964023238888dfc27558f145779124c3815f72f
show kawazoe <show753@icloud.com>
2022-04-18 10:00:46 +0900

    create psge

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,32 @@
 const path = require("path")
 
-export.createPages = async ({ graphql, actions, reporter }) => {
+exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
+
+  const blogresult = await graphql(`
+    query {
+			allContentfulBlogPost(
+				sort: {fields: publishDate, order: DESC}
+			) {
+				edges {
+					node {
+						id
+						slug
+					}
+				}
+			}
+		}
+  `)
+
+	if (blogresult.errors) {
+		reporter.panicOnBuild(`GraphQLのクエリでエラーが発生しました`)
+		return
+	}
+
+	blogresult.data.allContentfulBlogPost.edges.forEach(({ node }) => {
+		createPage({
+			path: `/blog/post/${node.slug}/`,
+			component: path.resolve(`./src/templates/blogpost-template.js`),
+		})
+	})
 }
